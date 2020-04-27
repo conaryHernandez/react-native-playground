f83ad32feb248564052c81c82d2fbac3f70eab20
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-04-27 15:48:05 -0600

    shop app: adding items to the cart

--- shop-app/screens/shop/ProductsOverviewScreen.js
+++ shop-app/screens/shop/ProductsOverviewScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FlatList, Text } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ProductItem from '../../components/shop/ProductItem';
+import * as cartActions from '../../store/actions/cart';
 
 const ProductsOverviewScreen = ({ navigation }) => {
   const products = useSelector((state) => state.products.availableProducts);
+  const dispatch = useDispatch();
 
   return (
     <FlatList
@@ -14,7 +16,9 @@ const ProductsOverviewScreen = ({ navigation }) => {
           image={itemData.item.imageUrl}
           title={itemData.item.title}
           price={itemData.item.price}
-          onAddCart={() => {}}
+          onAddCart={() => {
+            dispatch(cartActions.addToCart(itemData.item));
+          }}
           onViewDetail={() => {
             navigation.navigate('ProductDetails', {
               productId: itemData.item.id,
---
+++ shop-app/store/reducers/cart.js
@@ -0,0 +1,40 @@
+import { ADD_TO_CART } from '../actions/cart';
+import CartItem from '../../models/cart-item';
+
+const initialState = {
+  items: {},
+  totalAmount: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const addedProduct = action.product;
+      const prodPrice = addedProduct.price;
+      const prodTitle = addedProduct.title;
+
+      let updatedOrNweCartItem;
+
+      if (state.items[addedProduct.id]) {
+        updatedOrNweCartItem = new CartItem(
+          state.items[addedProduct.id].quantity + 1,
+          prodPrice,
+          prodTitle,
+          state.items[addedProduct.id].sum + prodPrice
+        );
+      } else {
+        updatedOrNweCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+      }
+
+      return {
+        ...state,
+        items: { ...state.items, [addedProduct.id]: updatedOrNweCartItem },
+        totalAmount: state.totalAmount + prodPrice,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
