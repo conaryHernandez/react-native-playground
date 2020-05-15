c1e0b56f8f49671c4330365cf8b50efe9187ebda
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-05-14 19:14:15 -0600

    otp
    creating users

---
+++ one-time-password/functions/createUser.js
@@ -0,0 +1,20 @@
+const admin = require('firebase-admin');
+
+const createUser = async (req, res) => {
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+
+  const phone = String(req.body.phone).replace(/[^\d]/g);
+
+  try {
+    const user = await admin.auth().createUser({
+      uid: phone,
+    });
+    res.send(user);
+  } catch (error) {
+    res.status(422).send({ error });
+  }
+};
+
+module.exports = createUser;
--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -1,11 +1,12 @@
 const functions = require('firebase-functions');
+const createUser = require('./createUser');
+const admin = require('firebase-admin');
+const serviceAccount = require('./service_account.json');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://one-time-password-eceb2.firebaseio.com',
 });
+
+exports.createUser = functions.https.onRequest(createUser);
