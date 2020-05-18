d5d78b6d9b3858a505d48291a8e5116e51fd5617
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-05-17 20:07:36 -0600

    jobs
    initial screens and added react navigation

---
+++ jobs-app/screens/DeckScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DeckScreen = () => {
+  return (
+    <View>
+      <Text>The DeckScreen</Text>
+    </View>
+  );
+};
+
+export default DeckScreen;
---
+++ jobs-app/screens/MapScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>The MapScreen</Text>
+    </View>
+  );
+};
+
+export default MapScreen;
