d66d4765b78aa4423e43ccd2388b5a8c1809a880
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-05-22 16:10:00 -0600

    jobs
    facebook auth initialization

--- jobs-app/screens/AuthScreen.js
+++ jobs-app/screens/AuthScreen.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { View, Text } from 'react-native';
 
+import * as actions from '../store/actions';
+
 const AuthScreen = () => {
+  dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(actions.facebookLogin());
+  }, [dispatch, actions]);
+
   return (
     <View>
       <Text>The Auth Screen</Text>
--- jobs-app/store/actions/auth_actions.js
+++ jobs-app/store/actions/auth_actions.js
@@ -1,4 +1,39 @@
 import { AsyncStorage } from 'react-native';
-import { FACEBOOK_LOGIN_SUCCESS } from './types';
+import * as Facebook from 'expo-facebook';
+import { FACEBOOK_LOGIN_SUCCESS, FACEBOOK_LOGIN_FAIL } from './types';
+import keys from '../../keys';
 
-export const facebookLogin = () => {};
+const doFacebookLogin = async (dispatch) => {
+  await Facebook.initializeAsync(keys.appId);
+
+  let { type, token } = await Facebook.logInWithReadPermissionsAsync(
+    keys.appId,
+    {
+      permissions: ['public_profile'],
+    }
+  );
+
+  if (type === 'cancel') {
+    return dispatch({
+      type: FACEBOOK_LOGIN_FAIL,
+    });
+  }
+
+  await AsyncStorage.setItem('fb_token', token);
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS });
+};
+
+export const facebookLogin = () => {
+  return async (dispatch) => {
+    let token = await AsyncStorage.getItem('fb_token');
+
+    if (token) {
+      dispatch({
+        type: FACEBOOK_LOGIN_SUCCESS,
+        payload: token,
+      });
+    } else {
+      doFacebookLogin(dispatch);
+    }
+  };
+};
