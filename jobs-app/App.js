2173860345e76c58125671e04eccdb85a66e56f3
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-05-28 22:05:34 -0600

    jobs
    push notifications

--- jobs-app/App.js
+++ jobs-app/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MainNavigator from './Navigator';
-
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import { Provider } from 'react-redux';
-import reduxsConfig from './store';
 
-const { store } = reduxsConfig();
+import MainNavigator from './Navigator';
+import store from './store';
+import registerForNotifications from './services/push_notifications';
+import { Notifications } from 'expo';
 
 export default function App() {
+  useEffect(() => {
+    registerForNotifications();
+    Notifications.addListener((notification) => {
+      const {
+        data: { text },
+        origin,
+      } = notification;
+
+      if (origin === 'received' && text) {
+        Alert.alert('New Push Notification', text, [{ text: 'Ok' }]);
+      }
+    });
+  }, [registerForNotifications]);
+
   return (
     <Provider store={store}>
       <MainNavigator />
--- jobs-app/store/index.js
+++ jobs-app/store/index.js
@@ -1,23 +1,9 @@
-import { AsyncStorage } from 'react-native';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { persistStore, persistReducer } from 'redux-persist';
 import reducers from './reducers';
 
-export default () => {
-  const store = createStore(reducers, {}, compose(applyMiddleware(thunk)));
-
-  let persistor = persistStore(store, {
-    store: AsyncStorage,
-    whitelist: ['likedJobs'],
-  });
-  return { store, persistor };
-};
-
-/*const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-};
-
-const persistedReducer = persistReducer(persistConfig, reducers);
-*/
+export default store = createStore(
+  reducers,
+  {},
+  compose(applyMiddleware(thunk))
+);
